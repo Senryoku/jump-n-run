91bdae3e23e1afff30c4f7869a1b7504a87470a9
Senryoku <maretverdant@gmail.com>
2012-03-20 16:56:07 +0000

    Ca marche mieux sans en fait x)

--- src/Physics/World.cpp
+++ src/Physics/World.cpp
@@ -204,13 +204,6 @@ void wdHandleCollision(World* W)
 								PositionOnEdge = (vxGetPosition(Info.V).y - CollisionVector.y
 							- PosE1.y)/(PosE2.y - PosE1.y);
 
-							/* Cas particulier où la détection de collision n'est pas valide */
-							if(PositionOnEdge > 1.f || PositionOnEdge < 0.f)
-							{
-								it2 = nodeGetNext(it2);
-								continue;
-							}
-
 							/* DEBUG !
 							if (DebugDraw)
 							{
