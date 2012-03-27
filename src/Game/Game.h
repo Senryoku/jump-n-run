cab3cfbf2dc315a692a46fbe4bd739d7f5b2fdc8
Senryoku <maretverdant@gmail.com>
2012-03-27 09:57:26 +0000

    Game

---
+++ src/Game/Game.h
@@ -0,0 +1,20 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+
+/** @defgroup Game
+ *
+ * Structure du jeu
+ * Préfixe des méthodes : gm
+ **/
+
+void gmMenu();
+
+void gmLaunch();
+
+void gmLvlEd();
+
+/** @}
+**/
+
+#endif
+
