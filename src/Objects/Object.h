ccff81279fb99de3fb5af4a1646febcbfa379ec3
Senryoku <maretverdant@gmail.com>
2012-04-01 18:35:52 +0000

    De quoi discuter demain - Avec les fichiers.

---
+++ src/Objects/Object.h
@@ -0,0 +1,24 @@
+#ifndef _OBJECT_H_
+#define _OBJECT_H_
+
+#include <Physics/Polygon.h>
+
+/** @defgroup Object
+ *
+ * Préfixe des méthodes : ??
+ **/
+
+/** @brief Objet dynamique afichable (texturé)
+ *
+ * @todo Réfléchir à la façon de binder la texture (Liste de coordonnée ?
+ * Une par Vexter de Shape ? Toujours 4 (Shape = Rectangle) ?...)
+**/
+typedef struct {
+	Polygon* Shape; /**< Polygon de collision **/
+	unsigned int Texture; /**< Numero de texture, relatif au niveau **/
+} Object;
+
+/** @}
+**/
+
+#endif
