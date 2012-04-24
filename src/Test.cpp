5f3061a2d6daefedefb284eb10f64a5e6181ba71
Senryoku <maretverdant@gmail.com>
2012-04-24 06:32:42 +0000

    Tiens, maintenant ça marche...

--- src/Test.cpp
+++ src/Test.cpp
@@ -13,18 +13,7 @@ int main(int argc, char** argv)
 	scFree(&Sc);
 	*/
 
-	DynArr DA;
-	unsigned int ErrorCode = scCollect(&DA, "tmpEditor.lvl", "3557a855ba37d9b60bc18583d99eb254");
-	if(ErrorCode == 0)
-	{
-		printf("Number of Score(s) : %u\n", daGetSize(&DA));
-		for(unsigned int i = 0; i < daGetSize(&DA); i++)
-		{
-			Score* S = (Score*) daGet(&DA, i);
-			printf("Player : %s, Hour : %s, Date : %s, Time : %u\n", S->Player, S->LvlName, S->LvlMD5, S->Time);
-		}
-		scCollectFree(&DA);
-	}
+	scRegressionTest();
 
 	LevelEditorApp App;
 
